e6814a79be0675cb50fdaab772dd4d69114605cb
Deepamageswari <78834310+Deepamageswari@users.noreply.github.com>
2023-08-24 10:48:21 +0530

    Update streamlit_app.py
    connecting

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,5 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+# requirements.txt
+import snowflake.connector
